7908915d721a429bc81572579548daca4ed3e864
Andreas Eger <dev@eger-andreas.de>
2012-11-27 12:23:44 +0100

    refactored the doe_search

--- lib/svm_toolkit/doe_search.rb
+++ lib/svm_toolkit/doe_search.rb
@@ -2,17 +2,17 @@ require 'celluloid'
 module SvmToolkit
   class Svm
     def self.doe_search(args={})
-      Celluloid::Actor[:search] = Search.new args.fetch(:folds) { 3 }
-      Celluloid::Actor[:search].search *args
+      doe_search = DoeSearch.new args.fetch(:folds) { 3 }
+      doe_search.search *args
     end
   end
-  class Search
-    include Celluloid
+  class DoeSearch
     attr_accessor :number_of_folds
     def initialize number_of_folds
       self.number_of_folds = number_of_folds
     end
     def search(args={})
+      # deal with input
       feature_vectors = args[:feature_vectors]
       cost_min = args.fetch(:cost_min) { -5 }
       cost_max = args.fetch(:cost_max) { 15 }
@@ -21,39 +21,57 @@ module SvmToolkit
       evaluator = params.fetch(:evaluator, Evaluator::OverallAccuracy)
       max_iterations = params.fetch(:interations) { 1 }
 
+      # split feature_vectors into folds
       *folds,_ = feature_vectors.each_slice(feature_vectors.size/number_of_folds).map{|set|
                    Problem.from_array(set.map(&:data), set.map(&:label))
                  }
 
-      worker = SvmWorker.pool(args: [ {evaluator: evaluator}] )
-
+      # initialize iteration parameters
       results = {}
-      parameter_pairs = build_doe_matrix cost_min, cost_max, gamma_min, gamma_max
       resolution = [(cost_min.abs + cost_max.abs)/2.0, (gamma_min.abs + gamma_max.abs)/2.0]
+      parameter_pairs = build_doe_matrix cost_min, cost_max, gamma_min, gamma_max
 
-      last_best = 0
+      # create Celluloid Threadpool
+      worker = SvmWorker.pool(args: [ {evaluator: evaluator}] )
       max_iterations.times do |iteration|
-        Celluloid::Actor[:collector] = SvmCollector.new
-
+        futures = []
         parameter_pairs.each do |cost,gamma|
           next if results.has_key?(cost: cost, gamma: gamma) # skip already tested models
           folds.each.with_index do |fold,index|
-            worker.train!(fold, Parameter.new(
-              :svm_type => Parameter::C_SVC,
-              :kernel_type => Parameter::RBF,
-              :cost => cost,
-              :gamma => gamma
-            ), folds.select.with_index{|e,ii| index!=ii } })
+            # train SVM async
+            futures << worker.future.train fold,
+                                  Parameter.new(:svm_type => Parameter::C_SVC,
+                                                :kernel_type => Parameter::RBF,
+                                                :cost => cost,
+                                                :gamma => gamma),
+                                  folds.select.with_index{|e,ii| index!=ii }
+            }
           end
         end
 
-        new_results = wait :collecting
+        # blocking call to receive all results
+        new_results = Hash.new { |h, k| h[k] = [] }
+        futures.map { |f|
+          result = f.value
+          new_results[cost: model.cost, gamma: model.gamma] << result: result
+        }
+        # make means of all folds
+        new_results = new_results.map{|k,v| {k => v.instance_eval { reduce(:+) / size.to_f }}}
+        new_results = Hash[*mean_results.map(&:to_a).flatten]
         results.merge new_results
 
         best_pair = results.invert[results.values.max] # get the key with the best value
         resolution.map!{|e| e/Math.sqrt(2)}
         parameter_pairs = build_doe_pattern_for_center best_pair[:cost], best_pair[:gamma], resolution
       end
+
+      best_pair = results.invert[results.values.max]
+      # retrain the model with the best results
+      feature_set = Problem.from_array(feature_vectors.map(&:data), feature_vectors.map(&:label))
+      model = Svm.svm_train(train, Parameter.new(:svm_type => Parameter::C_SVC,
+                                                :kernel_type => Parameter::RBF,
+                                                :cost => best_pair[:cost],
+                                                :gamma => best_pair[:gamma]))
       return model, results
     end
 
@@ -73,7 +91,7 @@ module SvmToolkit
                           cost_center = (cost_max+cost_min)/2.0,
                           gamma_center = (gamma_max+gamma_min)/2.0
       # 1=max, -1=min, 0=center
-      #     3^d points          #      2^d points
+      #     3^d pattern         #      2^d pattern
       #   |                     #   |
       #  1| O   O   O           #  1|
       #   |                     #   |   O   O
@@ -98,53 +116,22 @@ module SvmToolkit
 
       costs3d.product(gammas3d).concat(costs2d.product(gammas2d))
     end
-
-    def finished_collecting *args
-      signal :collecting, *args
-    end
   end
 
   private
   class SvmWorker
     include Celluloid
-    def initialize args
+    def initialize args={}
       @evaluator = args[:evaluator]
     end
     def train train, parameter, folds
-      Celluloid::Actor[:collector].one_more
-      evaluate Svm.svm_train(train, parameter),folds
+      evaluate Svm.svm_train(train, parameter), folds
     end
     def evaluate model, folds
       result = folds.map{ |fold|
         model.evaluate_dataset(fold, :evaluator => @evaluator)
       }.reduce(&:+) / folds.count
-      Celluloid::Actor[:collector].collect(model, result)
-    end
-  end
-  class SvmCollector
-    include Celluloid
-    attr_accessor :count
-    attr_accessor :results
-    attr_accessor :model
-    attr_accessor :lowest_error
-    attr_reader :recieved
-
-    def initialize args
-      @count = 0
-      @results = Hash.new { |h, k| h[k] = [] } #autoexpand hash
-      @recieved = 0
-    end
-    def one_more
-      count += 1
-    end
-    def collect model, result
-      @recieved+=1
-      results[cost: model.cost, gamma: model.gamma] << result: result
-      if recieved == count && count > 0
-        mean_results = results.map{|k,v| {k => v.instance_eval { reduce(:+) / size.to_f }}}
-        mean_results = Hash[*mean_results.map(&:to_a).flatten]
-        Celluloid::Actor[:search].finished_collecting mean_results
-      end
+      return [model, results]
     end
   end
 end
