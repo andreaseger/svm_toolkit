abbe5e3b5e97aba6e1f6cd255962d227ed42d307
Andreas Eger <dev@eger-andreas.de>
2012-12-06 11:21:55 +0100

    add correct licence too all files

--- lib/svm_toolkit/model.rb
+++ lib/svm_toolkit/model.rb
@@ -1,3 +1,22 @@
+# This file is part of svm_toolkit.
+#
+# Author::    Peter Lane
+# Copyright:: Copyright 2011, Peter Lane.
+# License::   GPLv3
+#
+# svm_toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# svm_toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with svm_toolkit.  If not, see <http://www.gnu.org/licenses/>.
+
 module SvmToolkit
   class Model
     # Evaluate model on given data set (an instance of Problem), 
